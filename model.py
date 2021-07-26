cbbcaa97cdc41a20c3e8e4dd5bbc7950259cfcea
Sarah Sawadogo <67437775+SarahSawadogo@users.noreply.github.com>
2021-07-26 16:39:31 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,13 @@
 import requests
 # # import random
 # # response = requests.get(giphy_request_link.json())
-
 def worldList(country):
     response = requests.get('https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/factbook.json').json()
-    print(response[3]['name'])
+    return response["countries"][country]
 #iterate through the list until the value for name in the dictionary matches the user search
-    for entry in response:
-        if entry['name'] == country:
-            return entry
-
-print(worldList('Japan'))
-    
-    
-
+    # for entry in response:
+    #     if entry['afghanistan'] == country:
+    #         return entry
+(worldList('japan'))
 # # for entry in range(3):
 # #     print(response[random.randint(0, 100)]["images"]["fixed_height"]["url"])
