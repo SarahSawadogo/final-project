17fffbf1de96fa0dda5ff7bc2dfb6845f7dbee13
Sarah <sawadogosarah@gmail.com>
2021-07-27 14:25:27 +0000

    10:25

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import os
 from model import worldList
+#  worldImageList
 # -- Initialization section --
 app = Flask(__name__)
 app.config["GIPHY_KEY"] = os.getenv("GIPHY_KEY")
@@ -18,7 +19,14 @@ def index():
 def countries_page():
     selectedCountry = request.form['country'].lower()
     raw = worldList(selectedCountry)
+
     return render_template("countries_page.html", time = datetime.now(), raw = raw)
+
+# def countries_image():
+#     selectedCountries = request.form['country']
+#     info = worldImageList(selectedCountries)
+
+#     return render_template("countries_page.html", time = datetime.now(), info = info)
 # def countries_page_img():
 @app.route('/maps.html')
 def maps():
@@ -32,3 +40,78 @@ def references():
 @app.route('/signIn.html')
 def signIn():
     return render_template("signIn.html", time = datetime.now())
+
+# city image routes    
+@app.route('/argentina.html')
+def argentina():
+    return render_template("argentina.html", time = datetime.now())
+
+@app.route('/australia.html')
+def australia():
+    return render_template("australia.html", time = datetime.now())
+
+@app.route('/cambodia.html')
+def cambodia():
+    return render_template("cambodia.html", time = datetime.now())
+
+@app.route('/canada.html')
+def canada():
+    return render_template("canada.html", time = datetime.now())
+
+@app.route('/chile.html')
+def chile():
+    return render_template("chile.html", time = datetime.now())
+
+@app.route('/fiji.html')
+def fiji():
+    return render_template("fiji.html", time = datetime.now())
+
+@app.route('/mexico.html')
+def mexico():
+    return render_template("mexico.html", time = datetime.now())
+
+@app.route('/morocco.html')
+def morocco():
+    return render_template("morocco.html", time = datetime.now())
+
+@app.route('/netherland.html')
+def netherland():
+    return render_template("netherland.html", time = datetime.now())
+
+@app.route('/newZealand.html')
+def newZealand():
+    return render_template("newZealand.html", time = datetime.now())
+
+@app.route('/nicaragua.html')
+def nicaragua():
+    return render_template("nicaragua.html", time = datetime.now())
+
+@app.route('/phillipines.html')
+def phillipines():
+    return render_template("phillipines.html", time = datetime.now())
+
+@app.route('/saudiArabia.html')
+def saudiArabia():
+    return render_template("saudiArabia.html", time = datetime.now())
+
+@app.route('/somalia.html')
+def somalia():
+    return render_template("somalia.html", time = datetime.now())
+
+@app.route('/south_africa.html')
+def south_africa():
+    return render_template("south_africa.html", time = datetime.now())
+
+@app.route('/switzerland.html')
+def switzerland():
+    return render_template("switzerland.html", time = datetime.now())
+
+@app.route('/ukraine.html')
+def ukraine():
+    return render_template("ukraine.html", time = datetime.now())
+
+@app.route('/uruguay.html')
+def uruguay():
+    return render_template("uruguay.html", time = datetime.now())
+
+
--- model.py
+++ model.py
@@ -4,6 +4,18 @@ import requests
 def worldList(country):
     response = requests.get('https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/factbook.json').json()
     return response["countries"][country]
+
+
+# def worldImageList(country):
+#     response = requests.get('https://restcountries.eu/rest/v2/all').json()
+#     return response["countries"][country]
+#iterate through the list until the value for name in the dictionary matches the user search
+    # for entry in response:
+    #     if entry['afghanistan'] == country:
+    #         return entry
+(worldList('japan'))
+# # for entry in range(3):
+# #     print(response[random.randint(0, 100)]["images"]["fixed_height"]["url"])
 #iterate through the list until the value for name in the dictionary matches the user search
     # for entry in response:
     #     if entry['afghanistan'] == country:
